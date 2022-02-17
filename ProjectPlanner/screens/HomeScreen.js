1cbacbed4d59c41f8dbf3b78405a1c43a9a2b03d
Matthieu Olislaegers <55691428+Matthieu1999@users.noreply.github.com>
2022-02-17 22:10:50 +0100

    Sign out

--- ProjectPlanner/screens/HomeScreen.js
+++ ProjectPlanner/screens/HomeScreen.js
@@ -1,14 +1,53 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 import React from 'react'
+import { auth } from '../firebase'
+import { signOut } from 'firebase/auth'
+import { useNavigation } from '@react-navigation/native'
 
 const HomeScreen = () => {
+
+  const navigation = useNavigation()
+
+  const handleSignOut = () => {
+    signOut(auth)
+      .then(() => {
+        navigation.replace("Login")
+      })
+      .catch(error => alert(error.message))
+  }
+
   return (
-    <View>
-      <Text>Home Screen</Text>
+    <View style={styles.container}>
+      <Text>Email: {auth.currentUser?.email} </Text>
+      <TouchableOpacity
+      onPress={handleSignOut}
+      style={styles.button}
+      >
+        <Text style={styles.buttonText}>Sign Out</Text>
+      </TouchableOpacity>
     </View>
   )
 }
 
 export default HomeScreen
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  button: {
+    backgroundColor: 'blue',
+    width: '60%',
+    padding: 15,
+    borderRadius: 10,
+    alignItems: 'center',
+    marginTop: 40,
+  },
+  buttonText: {
+    color: 'white',
+    fontWeight: '700',
+    fontSize: 16,
+  },
+})
