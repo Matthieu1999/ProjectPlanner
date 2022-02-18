5807dd29cd4a7b6f834e06a7f72a147782f75e87
Matthieu Olislaegers <55691428+Matthieu1999@users.noreply.github.com>
2022-02-18 22:45:24 +0100

    Update accountScreen.js
    Create account screen

--- ProjectPlanner/screens/accountScreen.js
+++ ProjectPlanner/screens/accountScreen.js
@@ -1,7 +1,7 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 
-const accountScreen = () => {
+const AccountScreen = () => {
   return (
     <View>
       <Text>accountScreen</Text>
@@ -9,6 +9,6 @@ const accountScreen = () => {
   )
 }
 
-export default accountScreen
+export default AccountScreen
 
 const styles = StyleSheet.create({})
