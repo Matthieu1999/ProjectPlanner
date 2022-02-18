1cc74ef8dc29fe44258ce16ac4e6eea203b84142
Matthieu Olislaegers <55691428+Matthieu1999@users.noreply.github.com>
2022-02-18 20:18:08 +0100

    Update App.js
    Centering header title of the home screen

--- ProjectPlanner/App.js
+++ ProjectPlanner/App.js
@@ -13,7 +13,16 @@ export default function App() {
     <NavigationContainer>
       <Stack.Navigator>
         <Stack.Screen options={{ headerShown: false}} name="Login" component={LoginScreen} />
-        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen options={{ 
+        title: 'Home',
+        headerTitleAlign: 'center',
+        headerStyle: {
+          backgroundColor: 'white',
+        },
+        headerTitleStyle: {
+          fontWeight: 'bold',
+        },
+        }} name="Home" component={HomeScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
