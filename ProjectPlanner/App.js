42d53a30ec93423eca79eb6609c8993570536417
Matthieu Olislaegers <55691428+Matthieu1999@users.noreply.github.com>
2022-02-19 00:52:40 +0100

    Update App.js
    Adding sign out button to every header

--- ProjectPlanner/App.js
+++ ProjectPlanner/App.js
@@ -3,7 +3,7 @@ import 'react-native-gesture-handler';
 import { StatusBar } from 'expo-status-bar';
 import { Button, StyleSheet, Text, View } from 'react-native';
 
-import { NavigationContainer } from '@react-navigation/native';
+import { NavigationContainer, useNavigation } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import LoginScreen from './screens/LoginScreen';
@@ -13,33 +13,69 @@ import ProjectScreen from './screens/ProjectScreen';
 import SettingsScreen from './screens/SettingsScreen';
 import RegisterScreen from './screens/RegisterScreen';
 
+import { auth } from './firebase'
+import { signOut } from 'firebase/auth'
+
 
 const Stack = createNativeStackNavigator();
 const Drawer = createDrawerNavigator();
 
 
 function MyDrawer() {
+
+  const navigation = useNavigation()
+  const handleSignOut = () => {
+    signOut(auth)
+      .then(() => {
+        navigation.navigate("Login")
+      })
+      .catch(error => alert(error.message))
+  }
+
   return (
     <Drawer.Navigator initialRouteName="Home">
+
       <Drawer.Screen options={{
         headerTitleAlign: 'center',
         title:"Home",
         headerRight: () => (
           <Button
-          onPress = {('')}
+          onPress = {(handleSignOut)}
           title={'Sign out'}
           />
-        )
+        ),
       }} name="Root" component={HomeScreen} />
+
       <Drawer.Screen options={{
         headerTitleAlign: 'center',
+        headerRight: () => (
+          <Button
+          onPress = {(handleSignOut)}
+          title={'Sign out'}
+          />
+        ),
       }} name="Projects" component={ProjectScreen} />
+
       <Drawer.Screen options={{
         headerTitleAlign: 'center',
+        headerRight: () => (
+          <Button
+          onPress = {(handleSignOut)}
+          title={'Sign out'}
+          />
+        ),
       }} name="Account" component={AccountScreen} />
+
       <Drawer.Screen options={{
         headerTitleAlign: 'center',
+        headerRight: () => (
+          <Button
+          onPress = {(handleSignOut)}
+          title={'Sign out'}
+          />
+        ),
       }} name="Settings" component={SettingsScreen} />
+
     </Drawer.Navigator>
   );
 }
