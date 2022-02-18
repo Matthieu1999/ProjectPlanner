ed742edf46b508d1a0c1f9c13d71b6fb8a6bc824
Matthieu Olislaegers <55691428+Matthieu1999@users.noreply.github.com>
2022-02-18 22:45:34 +0100

    Create ProjectScreen.js

---
+++ ProjectPlanner/screens/ProjectScreen.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const ProjectScreen = () => {
+  return (
+    <View>
+      <Text>ProjectScreen</Text>
+    </View>
+  )
+}
+
+export default ProjectScreen
+
+const styles = StyleSheet.create({})
