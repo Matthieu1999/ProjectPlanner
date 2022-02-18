d480251ff38a9a27d4ad53fa21cba3ab62a916e1
Matthieu Olislaegers <55691428+Matthieu1999@users.noreply.github.com>
2022-02-18 22:05:39 +0100

    Create accountScreen.js

---
+++ ProjectPlanner/screens/accountScreen.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const accountScreen = () => {
+  return (
+    <View>
+      <Text>accountScreen</Text>
+    </View>
+  )
+}
+
+export default accountScreen
+
+const styles = StyleSheet.create({})
