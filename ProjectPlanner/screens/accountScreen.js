91ded9a659e2db6a2dbd83a040020d31edda7278
Matthieu Olislaegers <55691428+Matthieu1999@users.noreply.github.com>
2022-03-09 17:08:48 +0100

    Update accountScreen.js

--- ProjectPlanner/screens/accountScreen.js
+++ ProjectPlanner/screens/accountScreen.js
@@ -1,19 +1,48 @@
 import 'react-native-gesture-handler';
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, KeyboardAvoidingView } from 'react-native'
 import React from 'react'
 import { auth } from '../firebase'
+import { TouchableOpacity } from 'react-native-gesture-handler';
 
 const AccountScreen = () => {
   return (
-    <View>
-      <Text>Username:</Text>
-      <Text>Email: {auth.currentUser?.email} </Text>
-      <Text>Password: ********** </Text>
-    </View>
+    <KeyboardAvoidingView
+    style={styles.container}
+    behavior="padding"
+    >
+
+      <View style={styles.usernameContainer}>
+
+        <Text style={styles.itemText}>Username:</Text>
+
+        <TouchableOpacity>
+          <Text>Change Username</Text>
+        </TouchableOpacity>
+
+      </View>
+      
+      <Text style={styles.itemText}>Email: {auth.currentUser?.email} </Text>
+      
+      <Text style={styles.itemText}>Password: ********** </Text>
+
+
+    </KeyboardAvoidingView>
   )
 }
 
 
 export default AccountScreen
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+
+  container: {
+    flex: 1,
+    // alignItems: 'center',
+  },
+  usernameContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+
+
+})
