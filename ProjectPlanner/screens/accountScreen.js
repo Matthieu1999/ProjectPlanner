759c480a52794367f277864591cc637ca36cbb2f
Matthieu Olislaegers <55691428+Matthieu1999@users.noreply.github.com>
2022-03-16 19:18:49 +0100

    Update accountScreen.js

--- ProjectPlanner/screens/accountScreen.js
+++ ProjectPlanner/screens/accountScreen.js
@@ -1,10 +1,54 @@
 import 'react-native-gesture-handler';
-import { StyleSheet, Text, View, KeyboardAvoidingView } from 'react-native'
-import React from 'react'
-import { auth } from '../firebase'
-import { TouchableOpacity } from 'react-native-gesture-handler';
+import { StyleSheet, Text, View, KeyboardAvoidingView, TextInput, TouchableOpacity } from 'react-native'
+import React, { useState, useEffect } from 'react'
+import { auth, db } from '../firebase'
+import { addDoc, collection, query, where, getDocs, doc, updateDoc, getDoc } from 'firebase/firestore';
+
+import { onAuthStateChanged } from "firebase/auth";
 
 const AccountScreen = () => {
+
+  const [userName, setUserName] = useState('')
+  const [currentUser, setCurrentUser] = useState(null)
+
+  useEffect(() => {
+
+    onAuthStateChanged(auth, (user) => {
+      if (user !== null) {
+        getCurrentUser();
+      }
+    });
+  }, [])
+
+  async function getCurrentUser() {
+    if (auth.currentUser !== null) {
+      setCurrentUser(auth.currentUser);
+    }
+  }
+
+  async function updateUsername() {
+
+    console.log(currentUser.uid)
+    console.log(userName)
+
+    const displayNameFirestore = query(collection(db, "Users"), where("userId", "==", "uYL0MWg9ONhdG7FaNEko9UsTcpp2"))
+
+    await updateDoc(displayNameFirestore, {
+      displayName: userName,
+    });
+
+    // const userNameFirestore = await updateDoc(collection(db, "Users"), where("userId", "==", currentUser.uid)) 
+    // try {
+    //   userNameFirestore, {
+    //     displayName: userName,
+    //   }
+    //   console.log("Document written with ID: ", userNameFirestore.id);
+    // } catch (e) {
+    //   console.error("Error adding document: ", e);
+    // }
+    
+  };
+
   return (
     <KeyboardAvoidingView
     style={styles.container}
@@ -13,9 +57,18 @@ const AccountScreen = () => {
 
       <View style={styles.usernameContainer}>
 
-        <Text style={styles.itemText}>Username:</Text>
+        <Text style={styles.itemText}>Username: </Text>
 
-        <TouchableOpacity>
+        <TextInput
+        placeholder="Choose username"
+        value={userName}
+        onChangeText={text => setUserName(text)}
+        style={styles.input}
+        ></TextInput>
+
+        <TouchableOpacity
+        onPress={updateUsername}
+        >
           <Text>Change Username</Text>
         </TouchableOpacity>
 
@@ -42,6 +95,13 @@ const styles = StyleSheet.create({
   usernameContainer: {
     flexDirection: 'row',
     justifyContent: 'space-between',
+  },
+  input: {
+    backgroundColor: 'white',
+    paddingHorizontal: 15,
+    paddingVertical: 10,
+    borderRadius: 10,
+    marginTop: 5,
   },
 
 
