c4c049f0e94965d8699f978a0199d864e4e4993a
Matthieu Olislaegers <55691428+Matthieu1999@users.noreply.github.com>
2022-03-22 20:33:18 +0100

    Update LoginScreen.js

--- ProjectPlanner/screens/LoginScreen.js
+++ ProjectPlanner/screens/LoginScreen.js
@@ -34,7 +34,7 @@ const LoginScreen = () => {
     signInWithEmailAndPassword(auth, email, password)
       .then(userCredentials => {
         const user = userCredentials.user;
-        console.log('Logged in with: ', user.email);
+        // console.log('Logged in with: ', user.email);
         
       })
       .catch(error => alert(error.message))
