e78f50ea65136c11875cc16d69e9d86894d24e2b
Matthieu Olislaegers <55691428+Matthieu1999@users.noreply.github.com>
2022-03-22 20:33:26 +0100

    Create darkMode.js

---
+++ ProjectPlanner/styles/darkMode.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { StyleSheet } from "react-native";
+
+const darkMode = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#212529',
+        alignItems: 'center',
+        justifyContent: 'center',
+      },
+})
+
+export default darkMode;
