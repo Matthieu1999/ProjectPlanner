cab17fddfeaf8c624d5d57a0eef8ebb38e289e10
Matthieu Olislaegers <55691428+Matthieu1999@users.noreply.github.com>
2022-03-24 19:28:51 +0100

    Update SettingsScreen.js

--- ProjectPlanner/screens/SettingsScreen.js
+++ ProjectPlanner/screens/SettingsScreen.js
@@ -1,16 +1,62 @@
 import 'react-native-gesture-handler';
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View, Switch } from 'react-native'
+import React, { useState, useEffect } from 'react'
 
 const SettingsScreen = () => {
+
+  const [isEnabledDarkMode, setIsEnabledDarkMode] = useState(false);
+  const toggleSwitchDarkMode = () => setIsEnabledDarkMode(previousState => !previousState);
+
+  const [isEnabledNotifications, setIsEnabledNotifications] = useState(false);
+  const toggleSwitchNotifications = () => setIsEnabledNotifications(previousState => !previousState);
+
   return (
-    <View>
-      <Text>Dark Mode</Text>
-      <Text>Push Notifications</Text>
+    <View style={styles.container}>
+      <View style={styles.settingContainer}>
+        <Text style={styles.settingText}>Dark Mode</Text>
+        <Switch
+        style={styles.settingSwitch}
+        trackColor={{ false: "#767577", true: "#81b0ff" }}
+        thumbColor={isEnabledDarkMode ? "#f5dd4b" : "#f4f3f4"}
+        onValueChange={toggleSwitchDarkMode}
+        value={isEnabledDarkMode}
+        />
+      </View>
+      <View style={styles.settingContainer}>
+        <Text style={styles.settingText}>Push Notifications</Text>
+        <Switch
+        style={styles.settingSwitch}
+        trackColor={{ false: "#767577", true: "#81b0ff" }}
+        thumbColor={isEnabledNotifications ? "#f5dd4b" : "#f4f3f4"}
+        onValueChange={toggleSwitchNotifications}
+        value={isEnabledNotifications}
+        />
+      </View>
     </View>
   )
 }
 
 export default SettingsScreen
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+  },
+  settingContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    padding: 10,
+    borderBottomWidth: 1,
+    borderColor: 'gray',
+  },
+  settingText: {
+
+  },
+  settingSwitch: {
+
+  },
+
+})
